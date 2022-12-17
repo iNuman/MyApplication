5d8c305049762ee78ba8f484fd226ca482738e8f
Numan Ali <flopy6663@gmail.com>
2022-12-17 18:09:26 +0500

    Initial commit

---
+++ app/src/main/java/com/background/video/spy/recording/myapplication/CalculatorOperation.kt
@@ -0,0 +1,10 @@
+package com.background.video.spy.recording.myapplication
+
+import java.text.DateFormatSymbols
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("x")
+    object Divide: CalculatorOperation("/")
+}
